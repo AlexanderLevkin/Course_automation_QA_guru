dfe84623b194581331a5e69474cb35b39583a8a4
Alexander_Levkin <levkin.a@promail366.com>
2023-05-04 22:39:54 +0300

    add new lesson

--- lesson_2_tools_and_library/fixture_test.py
+++ lesson_2_tools_and_library/fixture_test.py
@@ -1,5 +1,19 @@
+import pytest
 
-def test_body():
+
+@pytest.fixture()
+def open_browser():
+    b = "browser"
+    yield "browser"
+
+
+@pytest.fixture()
+def create_user(open_browser):
+    return 35
+
+
+@pytest.fixture()
+def test_body(open_browser):
     pass
     # Перейти на страницу логина
     # Вывести логин и пароль
